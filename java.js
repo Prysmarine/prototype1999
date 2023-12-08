c24273e8a35521e9fdcf250046d3fc11a0efd958
Prysmarine <dtbrowitt@gmail.com>
2023-12-07 21:10:59 -0700

    array editing
    messing with find() findindex()

--- java.js
+++ java.js
@@ -15,44 +15,60 @@ function dim(x) {
 }
 
 function ownedAdjust(x) {
-    var pos;
-    var match = false;
     test = document.getElementById("testP");
     test.innerHTML = "Entered Adjust";
-    var i = units.length;
-    do{
-        test.innerHTML += "<br>Started While Loop";           
-        if(x == unit[i]) {
-            test.innerHTML += "<br>Match Found.";
-            match = true;
-            pos = i;
-            break;
+    var z;
+    if (owned.length == 0)
+    {
+        test.innerHTML += "<br>owned length is /" + owned.length;
+        owned.push(x);
+        test.innerHTML += "<br>owned length is now /" + owned.length;
+    }
+    else 
+    {
+        test.innerHTML += "<br>entered the else statement";
+        z = owned.findIndex(checkOwned, x)
+        test.innerHTML += "<br>" + z;
+        switch(z)
+        {
+            case -1:
+                test.innerHTML += "<br> case -1";
+                owned.push(x);
+                break;
+            default:
+                test.innerHTML += "<br> case default";
+                owned.splice(z, 1);
+                break;
         }
-    } while(i > 0)
-    test.innerHTML += "<br>Ended While Loop. Match status is: " + match;
-    if (match)
-    {
-        test.innerHTML += "<br>Entered Matching Removal."
-        unit.splice(i, 1);
-    }
-    else
-    {
-        unit.push(x);
-    }
-    test.innerHTML += "<br>Number of units in the array is: " + unit.length;
         
-    
+    }
     displayOwned();
 }
 
+function checkOwned(x)
+{
+    document.getElementById("testP").innerHTML += "<br>getting the index of unit";
+    return owned.x == this.x;
+    
+}
+
+
 function displayOwned() {
-    var i = 0;
+    i = 0;
     test = document.getElementById("testP");
-    test.innerHTML += "<br>Entered displayOwned<br>";
+    test.innerHTML += "<br>Entered displayOwned<br>owned.length is " + owned.length;
+    document.getElementById("unitList").innerHTML = "This will be where the units are listed.";
     while (i < owned.length)
     {
+        test.innerHTML += "<br>Entered display while loop. value is " + i + " " + owned.length;
+        if (owned.length == 0)
+        {
+            test.innerHTML += "<br> entered the IF statement";
+            document.getElementById("unitList").innerHTML = "no units";
+        }
+        test.innerHTML += "<br>passed the if statement";
         document.getElementById("unitList").innerHTML += owned[i];
-        i++
+        i++;
     }
     test.innerHTML += "<br>Finished displayOwned";
 }
